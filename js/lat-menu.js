c1fe864b5c1b475aa967b5b2f22ede1d2c5922cc
dangomez04 <133095193+dangomez04@users.noreply.github.com>
2024-04-18 21:27:36 +0200

    empezado contacto

--- js/lat-menu.js
+++ js/lat-menu.js
@@ -1,24 +1,24 @@
 $(document).ready(function() {
-    $('a#lateralMenu').on('click',function(){
+    $('a#lateralMenu').on('click', function() {
+        console.log("hola");
+
+        console.log("hola");
+
         var iconoActual = $('svg#list').html();
         var nuevoIcono;
 
         if (iconoActual.includes('M2.5')) {
             // si el icono actual del menu lateral es la hamburguesa:
-
             nuevoIcono = '<path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>';
-            //mostrar menu lateral
-
-            $("div.latMenu").toggle();
+            // mostrar menu lateral con animación de transición
+            $("div.latMenu").addClass('show-menu');
         } else {
-            nuevoIcono = 
-                '<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />';
-                //ocultar menu lateral
-                $("div.latMenu").toggle();
+            nuevoIcono = '<path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5m0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5" />';
+            // ocultar menu lateral con animación de transición
+            $("div.latMenu").removeClass('show-menu');
+        }
 
-            }
-
-        //asignamos el nuevo icono
+        // asignar el nuevo icono
         $('a#lateralMenu svg').html(nuevoIcono);
     });
 });
