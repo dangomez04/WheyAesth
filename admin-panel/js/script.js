d872a96398d77e8e109df18460e80cf38e38a1a2
dangomez04 <danielgomezgarciaaa@gmail.com>
2024-04-29 13:48:04 +0200

    acabado estilos accesorios

---
+++ admin-panel/js/script.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+
+
+    $("div#accesorios").on('click', function () {
+        location.href = "Accesorios.html";
+    });
+
+    $("div#suplementos").on('click', function () {
+        location.href = "Suplementos.html";
+    }); 
+    
+    $("div#entrenadores").on('click', function () {
+        location.href = "Entrenadores.html";
+    });
+    
+    $("div#reuniones").on('click', function () {
+        location.href = "Reuniones.html";
+    });
+    
+    $("div#reservas").on('click', function () {
+        location.href = "Reservas.html";
+    });
+    
+    $("div#ofertas").on('click', function () {
+        location.href = "Ofertas-flash.html";
+    });
+    
+    $("div#usuarios").on('click', function () {
+        location.href = "Usuarios.html";
+    }); 
+    
+    $("div#roles").on('click', function () {
+        location.href = "Roles.html";
+    });
+
+});
